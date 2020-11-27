f8b3aad11deae4c4a4763c379e68020af26ee4b6
cor3ntino <corentin.morvanchaumeil@gmail.com>
2020-11-27 19:59:54 +0100

    données clean

--- preprocessing_data.py
+++ preprocessing_data.py
@@ -7,7 +7,11 @@ data['Direction'] = data['Direction'].astype('category').cat.codes
 
 col = ['location_name', 'location_latitude', 'location_longitude', 'Year', 'Month', 'Day', 'Day of Week', 'Hour', 'Direction']
 data = data.groupby(col)['Volume'].sum().reset_index()
-print(data.head())
-data = data.pivot_table(index=['location_name', 'location_latitude', 'location_longitude', 'Year', 'Month', 'Day', 'Day of Week', 'Direction'], columns='Hour', values='Volume', fill_value=0).reset_index()
-print(data.head())
-print(data[0,:])
+
+data = data.pivot_table(index=['location_name', 'location_latitude', 'location_longitude', 'Year', 'Month', 'Day', 'Day of Week', 'Direction'], columns='Hour', values='Volume').reset_index()
+#print(data[(data['location_latitude'] <= 30.402286) & (data['Year'] == 2017) & (data['Month'] == 9) & (data['Direction'] == 1)])
+#data = data.interpolate('time')
+
+data.interpolate(method='linear', inplace=True) # après sa il ne reste que 2 lignes comprenant des valeurs NaN dans leur séries; nous allons les supprimer
+data = data.dropna()
+print(data[:20])
