379df448779b7c99e67d1e2330fb43e7af7b4cd8
cor3ntino <corentin.morvanchaumeil@gmail.com>
2020-12-10 05:02:44 +0100

    ok

--- test_seq_to_seq_LSTM.py
+++ test_seq_to_seq_LSTM.py
@@ -1,5 +1,7 @@
-import data_preprocessing_seq_to_seq
+import data_preprocessing
 import LSTM_seq_to_seq
+import CNN_seq_to_seq
+import visualisation
 
 import torch as torch
 import torch.nn as nn
@@ -7,22 +9,21 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-nb_days_before = 7
+input_window, output_window = 1, 1
+#train, test = data_preprocessing.process_data(input_window=input_window, output_window=output_window, file='./Radar_Traffic_Counts.csv')
 
-train, test = data_preprocessing_seq_to_seq.process_data(nb_days_before=nb_days_before, file='./Radar_Traffic_Counts.csv')
-
-data_train = torch.load('./data_seq_train_' + str(nb_days_before) + '.txt')
-data_test = torch.load('./data_seq_test_' + str(nb_days_before) + '.txt')
+data_train = torch.load('./data/data_train_' + str(input_window) + '_days_to_' + str(output_window) + '_hours.txt')
+data_test = torch.load('./data/data_test_' + str(input_window) + '_days_to_' + str(output_window) + '_hours.txt')
 
 n_train, n_test = data_train.shape[0], data_test.shape[0]
 batch_size = 128
-data_loader_train, data_loader_test = data_preprocessing_seq_to_seq.data_loader(data_train, data_test, batch_size=batch_size)
+data_loader_train, data_loader_test = data_preprocessing.data_loader(data_train, data_test, input_window, output_window, batch_size=batch_size)
 
-model = LSTM_seq_to_seq.LSTM_NN_seq_to_seq(longueur_forecast=24)
+model = LSTM_seq_to_seq.LSTM(input_window=input_window, output_window=output_window)
 error = nn.MSELoss()
-learning_rate = 0.0005
+learning_rate = 0.001
 weight_decay = 0.0001
-num_epoch = 10
+num_epoch = 1
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.50)
 
@@ -30,9 +31,9 @@ test_loss_list, pourcentage_loss_list = [], []
 
 count, pourcentage = 0, 0.
 for epoch in range(num_epoch):
-    for serie, target in data_loader_train:
+    for (day_of_week, serie), target in data_loader_train:
 
-        output = model.forward(serie)
+        output = model.forward(day_of_week, serie)
         target = target.float()
         loss = error(output, target)
         optimizer.zero_grad()
@@ -44,9 +45,9 @@ for epoch in range(num_epoch):
         if count >= pourcentage * n_train:
             test_loss_batch = []
 
-            for serie_t, target_t in data_loader_test:
+            for (day_of_week_t, serie_t), target_t in data_loader_test:
 
-                output_t = model.forward(serie_t)
+                output_t = model.forward(day_of_week_t, serie_t)
                 loss_test = error(output_t, target_t).data.item()
                 test_loss_batch.append(loss_test)
 
@@ -59,24 +60,23 @@ for epoch in range(num_epoch):
             pourcentage += 0.1
     scheduler.step()
 
-torch.save(model.state_dict(), './LSTM_seq.pt')
-
-#model = LSTM_seq_to_seq.LSTM_NN_seq_to_seq(longueur_forecast=1)
-#model.load_state_dict(torch.load('./LSTM_1.pt'))
-
+visualisation.forecast(model,1,1)
+visualisation.pred_vs_reality(model, 1, 1)
+"""
 L = [100, 200, 500, 1000, 2650, 5000, 10000]
 for k in L:
-    serie, target = torch.tensor([data_test[k][:-24]]), torch.tensor([data_test[k][-24:]])
-    pred = model.forward(serie)
+    day_week, serie, target = torch.tensor([data_test[k][:7]]), torch.tensor([data_test[k][7:-24]]), torch.tensor([data_test[k][-24:]])
+    pred = model.forward(day_week, serie)
     serie = serie.tolist()[0]
     target = target.tolist()[0]
     pred = pred.tolist()[0]
 
     x, y1, y2 = [x for x in range(len(serie + target))], serie + target, serie + pred
-    plt.plot(x, y2, 'r')
-    plt.plot(x, y1, 'g')
+    plt.plot(x, y2, 'r', label="Prédiction")
+    plt.plot(x, y1, 'g', label="Réalité")
+    plt.title("Data vs Pred")
     plt.show()
-
+"""
 """
 plt.figure(0)
 plt.plot(pourcentage_loss_list, test_loss_list)
